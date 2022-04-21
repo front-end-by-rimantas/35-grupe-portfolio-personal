fc14c56c6d1d9ebfbe17e1b80146b0e14fa34e6c
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-04-21 16:19:14 +0300

    new class name and better transition

--- js/components/plansList/plansList.js
+++ js/components/plansList/plansList.js
@@ -3,7 +3,7 @@ function renderPlans(selector, data) {
     let HTML = '';
     for (const item of data) {
         HTML += `
-        <div class="col-12 col-md-6 col-lg-3 text section-description">
+        <div class="text plan">
         <span class="circle">${item.number}</span>
         <h3 class="section-subtitle">${item.title}</h3>
         <p>${item.desc}</p>
@@ -11,6 +11,7 @@ function renderPlans(selector, data) {
         <p>${item.offer[1]}</p>
         <p>${item.offer[2]}</p>
         <h2 class="section-title">${item.currency} ${item.price}</h2>
+        <a href="#" class="buy-plan">Buy now</a>
       </div>`;
     }
 
