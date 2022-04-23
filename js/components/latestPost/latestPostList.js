24140287aee95ea712bc78aa7679543b14051bee
Kristina <crislayn@yahoo.com>
2022-04-23 09:06:23 +0300

    blog post links style

--- js/components/latestPost/latestPostList.js
+++ js/components/latestPost/latestPostList.js
@@ -3,14 +3,14 @@ function renderLatestpost(selector, data) {
 
   let html = '';
   for (const item of data) {
-    html += `<div class="col-12 col-md-4">
-    <div>
+    html += `<div class="col-12 col-md-4 blog">
+    <div class='links'>
     <img src="../../../img/blog-img${item.img}.webp" alt="${item.altText}" class="sizing">
-    <a href="#"><img src="../../../img/blog-${item.thumb}-img.webp">${item.link}</a>
+    <a href="#"><img src="../../../img/blog-${item.thumb}-img.webp"></a><a class='link'>${item.link}</a>
     <a href="#">${item.date}</a>
     </div>
-    <h4>${item.title}</h4>
-    <p>${item.desc}</p>
+    <h4 class='upper-heading'>${item.title}</h4>
+    <p class='section-description'>${item.desc}</p>
     </div>`;
   }
 
