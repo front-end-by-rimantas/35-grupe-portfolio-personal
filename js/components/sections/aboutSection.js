c53321731effa51902c83dddb38c9a5b5e3d3963
Paskanas <kaspe1992@gmail.com>
2022-04-25 16:48:22 +0300

    done Move aboutMe section logic to aboutSection.js #138

--- js/components/sections/aboutSection.js
+++ js/components/sections/aboutSection.js
@@ -6,14 +6,14 @@ const aboutSection = (selector) => {
     imgPath = '../.';
   }
   DOM.innerHTML = `
-      <div class="row">
+  <div class="row visible-flex about-container-row no-margin">
         <div class="col-12 col-md-6">
           <img src="${imgPath}./img/about-img.webp" alt="Me" class="sizing" />
         </div>
         <div class="col-12 col-md-6 col-lg-5 ml-lg-1">
-          <h3 class="section-headline left">ABOUT ME</h3>
-          <h2 class="section-title uppercase-smaller">PERSONAL DETAILS</h2>
-          <p class="section-description left">
+          <h3 class="section-headline uppercase aboutMe left">ABOUT ME</h3>
+          <h2 class="section-title about-me">PERSONAL DETAILS</h2>
+          <p class="section-description about-me left">
             Here, I focus on a range of items and features that we use in life
             without giving them a second thought. such as Coca Cola. Dolor sit
             amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
--- js/pages/home.js
+++ js/pages/home.js
@@ -11,7 +11,7 @@ import { plansData } from '../data/plansData.js';
 
 // import { heroSection } from '../components/sections/heroSection.js'
 // import { headerSection } from '../components/sections/headerSection.js';
-// import { aboutSection } from "../components/sections/aboutSection.js";
+import { aboutSection } from "../components/sections/aboutSection.js";
 // import { numbersCounterContainer } from '../components/sections/numberCounterSection.js';
 // import { testimonialsSection } from '../components/sections/testimonialsSection.js'
 // import { sponsorsSection } from '../components/sections/sponsorsSection.js'
@@ -43,7 +43,7 @@ addEventListener('scroll', function () {
 /* end of Hero (hero section) */
 
 /* AboutMe */
-// aboutSection('#about');
+aboutSection('#about');
 /* end of AboutMe */
 
 /* OfferedService */
