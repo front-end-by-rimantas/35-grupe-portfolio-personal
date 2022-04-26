cdae5f1d83a7102f45f5d60578adf94bee719ba3
Kristina Rudyte <rkristina32@yahoo.com>
2022-04-26 11:07:21 +0300

    hover

--- js/components/servicesList/servicesList.js
+++ js/components/servicesList/servicesList.js
@@ -4,7 +4,7 @@ const servicesList = (selector, data) => {
   for (const iterator of data) {
     htmlString += `<div class="col-12 col-md-6 col-lg-4 services-selector">
                     <span class="lnr lnr-${iterator.icon} service-icon"></span>
-                    <h2 class="section-smaller">${iterator.title}</h2>
+                    <h2 class="section-smaller serviceTitle-selector">${iterator.title}</h2>
                     <p class="section-description service">
                       ${iterator.description}
                     </p>
