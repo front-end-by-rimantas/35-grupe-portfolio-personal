141d5984f26c4957db402dfbd690dea1e4dfadeb
Paskanas <kaspe1992@gmail.com>
2022-04-26 14:44:17 +0300

    done Move Footer section logic to footerSection.js #146

--- js/pages/home.js
+++ js/pages/home.js
@@ -15,7 +15,7 @@ import { aboutSection } from "../components/sections/aboutSection.js";
 // import { numbersCounterContainer } from '../components/sections/numberCounterSection.js';
 import { testimonialsSection } from '../components/sections/testimonialsSection.js'
 // import { sponsorsSection } from '../components/sections/sponsorsSection.js'
-// import { footerSection } from '../components/sections/footerSection.js';
+import { footerSection } from '../components/sections/footerSection.js';
 import { projectsSection } from '../components/sections/projectsSection.js'
 // import { plansSection } from '../components/sections/plansSection.js'
 import { servicesSection } from '../components/sections/serviceSection.js';
@@ -75,5 +75,5 @@ postsSection('#posts');
 /* end of Sponsors */
 
 /* FOOTER */
-// footerSection('#footer');
+footerSection('#footer');
 /* end of FOOTER */
