d8bc1bf61cbb51546a9a481f7ea7a1d3ccf4e3f6
reginareg <regina.bogde@gmail.com>
2022-04-28 13:35:39 +0300

    numbering

--- js/components/numbersList/numbersList.js
+++ js/components/numbersList/numbersList.js
@@ -5,7 +5,7 @@ function numbersList(selector, data) {
         html += `
         <div class="row">
           <div class="col-12 col-md-6 col-lg-3">
-            <h2 class="section-headline center">${item.number}</h2>
+            <h2 class="section-headline center num">${item.number}</h2>
             <p class="section-description center">${item.desc}</p>
           </div>`;
     }
