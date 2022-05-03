58b4858dd85adf7e2dcfbe64ec5cd87c8c0533ba
Kristina <crislayn@yahoo.com>
2022-05-03 15:27:34 +0300

    a tag in div

--- js/components/sections/FAQSection.js
+++ js/components/sections/FAQSection.js
@@ -13,49 +13,49 @@ const FAQSection = (selector) => {
 </div>
 <div class="row">
   <div class="col-12 col-md-10 col-lg-8 ml-md-1 ml-lg-2 faq-list">
-  <div class="chevron">
-    <span class="lnr lnr-chevron-up"></span>
-    <span class="lnr lnr-chevron-down"></span>
-  </div>
-    <a href="#">Dude You Re Getting A Telescope</a>
+    <div class="question">
+      <span class="lnr lnr-chevron-up"></span>
+      <span class="lnr lnr-chevron-down"></span>
+      <a href="#">Dude You Re Getting A Telescope</a>
+    </div>
     <p class="section-description">
       Nunc placerat mi id nisi interdum mollis. Praesent pharetra, justo
       ut scelerisque mattis, leo quam aliquet diam, congue laoreet elit
       metus eget diam. Proin ac metus diam.
     </p>
-    <div class="chevron">
-    <span class="lnr lnr-chevron-up"></span>
-    <span class="lnr lnr-chevron-down"></span>
-  </div>
-    <a href="#">The Basics Of Buying A Telescope</a>
+    <div class="question">
+      <span class="lnr lnr-chevron-up"></span>
+      <span class="lnr lnr-chevron-down"></span>
+      <a href="#">The Basics Of Buying A Telescope</a>
+    </div>
     <p class="section-description">
       Donec nec justo eget felis facilisis fermentum. Aliquam porttitor
       mauris sit amet orci. Aenean dignissim pellentesque felis. leo quam
       aliquet diam, congue laoreet elit metus eget diam.
     </p>
-    <div class="chevron">
-    <span class="lnr lnr-chevron-up"></span>
-    <span class="lnr lnr-chevron-down"></span>
-  </div>
-    <a href="#">Astronomy Binoculars A Great Alternative</a>
+    <div class="question">
+      <span class="lnr lnr-chevron-up"></span>
+      <span class="lnr lnr-chevron-down"></span>
+      <a href="#">Astronomy Binoculars A Great Alternative</a>
+    </div>
     <p class="section-description">
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus
       hendrerit. Pellentesque aliquet nibh nec urna. Proin ac metus diam.
     </p>
-    <div class="chevron">
-    <span class="lnr lnr-chevron-up"></span>
-    <span class="lnr lnr-chevron-down"></span>
-  </div>
-    <a href="#">Do Your Self Realizations Quickly Fade</a>
+    <div class="question">
+      <span class="lnr lnr-chevron-up"></span>
+      <span class="lnr lnr-chevron-down"></span>
+      <a href="#">Do Your Self Realizations Quickly Fade</a>
+    </div>
     <p class="section-description">
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus
       hendrerit. Pellentesque aliquet nibh nec urna. Proin ac metus diam.
     </p>
-    <div class="chevron">
-    <span class="lnr lnr-chevron-up"></span>
-    <span class="lnr lnr-chevron-down"></span>
-  </div>
-    <a href="#">Success Steps For Your Personal Or Business Life</a>
+    <div class="question">
+      <span class="lnr lnr-chevron-up"></span>
+      <span class="lnr lnr-chevron-down"></span>
+      <a href="#">Success Steps For Your Personal Or Business Life</a>
+    </div>
     <p class="section-description">
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus
       hendrerit. Pellentesque aliquet nibh nec urna. Proin ac metus diam.
